009c84b93ca9bcc3c038391ccb43170a9667b6a3
zxcv1337x <michaelhaggard94@gmail.com>
2019-01-21 21:49:54 -0600

    omg i got it working finally

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from './components/header';
 import Post from './components/post'
 
 class App extends Component {
   render() {
-    return <div className="App">
+    return <Router>
+   <div className="App">
     <Header />
-
       <section className="App-main">
-      <Post username="Mike" avatar="https://i.imgur.com/Rqoc2Zh.jpg" caption="Here's a placeholder image." image="https://i.imgur.com/JynDHo7.jpg" />
-      <Post username="Mike" avatar="https://i.imgur.com/Rqoc2Zh.jpg" caption="#zxcv wins again!" image="https://i.imgur.com/ti6Kedj.png" />
-      <Post username="Ian" avatar="https://i.imgur.com/JMCi04X.jpg" caption=":)" image="https://i.imgur.com/0DuPjX6.jpg" />
+        <Route exact path="/" component={Post}/>
       </section>
 
     </div>
-
+    </Router>
   }
 }
 
