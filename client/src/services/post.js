d2786d5ad46032c1da5fb15ab438ef1e575f83d8
MichaelH94 <michaelhaggard94@gmail.com>
2019-01-24 21:55:09 -0600

    end of class update

--- client/src/services/post.js
+++ client/src/services/post.js
@@ -2,5 +2,6 @@ import api from './api.js'
 
 export default = {
     getPosts: () => axios.get('/api/post'),
-    add: (post) => axios.post('/api/post', post)
+    add: (post) => axios.post('/api/post', post),
+    update: (post) => api.put(`/post/${post.id}`, post)
 }
